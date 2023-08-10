cb4488a670e6f1b97c1262d2cc50b512c91ec635
FinlaySanders <121633244+FinlaySanders@users.noreply.github.com>
2023-08-10 15:26:24 +0100

    Update spin_model.py

--- spin_model.py
+++ spin_model.py
@@ -3,14 +3,14 @@ from torch.nn import Sequential as Seq, Linear as Lin, ReLU
 from torch_geometric.utils import scatter
 from torch_geometric.nn import MetaLayer
 from torch_geometric.loader import DataLoader
-from dataset import generate_spin_dataset
+from dataset import generate_spin_dataset, create_k_step_dataset
 
 class NodeModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.node_mlp_1 = Seq(Lin(3, 30), ReLU(), Lin(30, 30), ReLU(), Lin(30, 13))
+        self.node_mlp_1 = Seq(Lin(3, 15), ReLU(), Lin(15, 15), ReLU(), Lin(15, 10))
         # input is output of above+3
-        self.node_mlp_2 = Seq(Lin(16, 30), ReLU(), Lin(30, 30), ReLU(), Lin(30, 16), ReLU(), Lin(16, 3)) 
+        self.node_mlp_2 = Seq(Lin(13, 15), ReLU(), Lin(15, 30), ReLU(), Lin(30, 15), ReLU(), Lin(15, 3)) 
 
     def forward(self, x, edge_index, edge_attr, u, batch):
         # x: [N, F_x], where N is the number of nodes.
@@ -27,6 +27,10 @@ class NodeModel(torch.nn.Module):
         out = torch.cat([x, out], dim=1)
         out = self.node_mlp_2(out)
 
+        magnitude = torch.norm(out[:, :2], dim=1, keepdim=True)
+        out = torch.cat([out[:, :2] / magnitude, out[:, 2:]], dim=1)
+
+
         return out
 
 # creates, saves and validates a spin model 
@@ -38,29 +42,35 @@ def create_model():
     model.train()
 
     print("getting dataset")
-
-    dataset = generate_spin_dataset(30, 200, 200)
-    val_dataset = generate_spin_dataset(30, 50, 50, metropolis_sweeps=100)
-    #val_dataset = torch.load("val_data.pt") 
-
-    loader = DataLoader(dataset, batch_size=10, shuffle=True)
-    n_steps = len(dataset) / loader.batch_size
+    dataset = create_k_step_dataset(100, 100)
+        
+    n_steps = len(dataset)
 
     n_epochs = 1
     for epoch in range(n_epochs):
         batch_n = 1
-        for batch in loader:
-            
-            optimizer.zero_grad()
-            x, _, _ = model(x=batch.x, edge_index=batch.edge_index, edge_attr=batch.edge_attr, u=batch.u, batch=batch.batch)
+        for batch in dataset:
 
-            loss = loss_fn(x, batch.y)
+            initial_data_batch = batch[0]  # batch at beginning of sequence
+            ground_truths_batches = batch[1:] # batches at remaining timesteps in the sequnces
+
+            k = len(ground_truths_batches)
+
+            optimizer.zero_grad()
+            
+            loss = 0
+
+            prediction = initial_data_batch.x
+            for i in range(k):
+                prediction, _, _ = model(x=prediction, edge_index=initial_data_batch.edge_index, edge_attr=None, u=None, batch=initial_data_batch.batch)
+
+                loss += loss_fn(prediction, ground_truths_batches[i].x)
+
+            print("Batch ", batch_n, " out of ", n_steps, " Loss:", loss)
 
             loss.backward()
             optimizer.step()
 
-            if batch_n % (n_steps / 100) == 0:
-                print("Epoch: ", epoch, int(batch_n/n_steps*100), "%, Loss: ", loss)
             batch_n += 1
     
     torch.save(model.node_model.state_dict(), 'NodeModel.pt')
@@ -68,6 +78,7 @@ def create_model():
     model.eval()
 
     # Model Validation
+    val_dataset = generate_spin_dataset(30,50,50)
     loader = DataLoader(val_dataset, batch_size=10, shuffle=True)
 
     print("Test Dataset Size: ", len(dataset))
